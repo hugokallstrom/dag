f11f7cac9d9843e7a3327a5c812c64775b22802d
oi11hkm <oi11hkm@cs.umu.se>
2015-11-26 10:37:37 +0100

    Add background colors

--- js/dag.js
+++ js/dag.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
 	var days = ["Söndag...", "Måndag", "Tisdag", "Onsdag", "Torsdag", "Fredag!!", "Lördag!!"];
+	var colors = ["#88A825", "#CF4A30", "#ED8C2B", "#3BADA0", "#911146", "#AD2E0D", "#35203B"];
 	var d = new Date();
-	var today = days[d.getDay()];
-	document.getElementById("dayheader").innerHTML = today;
+	var today = d.getDay();
+	document.getElementById("dayheader").innerHTML = days[today];
+	document.body.style.backgroundColor = colors[today];
 });
 
